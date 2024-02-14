1cc880ba775bcffabe87edfca540d3b27af5b4bd
MYXAMOR338 <silova155@gmail.com>
2024-02-14 22:25:14 +0300

    Num pages Word correct

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def process_pdf(pdf_path, keywords, word_path):
     found_keywords = []
     found_date = None  # Здесь будем хранить найденную дату
 
+    start_page = 1  # Начальная страница текущего документа
+
     with fitz.open(pdf_path) as pdf:
         for page_num in range(len(pdf)):
             page = pdf.load_page(page_num)
@@ -33,7 +35,6 @@ def process_pdf(pdf_path, keywords, word_path):
                         print("Дата найдена:", date)
                         found_date = date 
 
-                
                 # Поиск текста в изображениях
                 for img_index, img in enumerate(images):
                     xref = img[0]
@@ -58,17 +59,19 @@ def process_pdf(pdf_path, keywords, word_path):
 
                 if "End" in text:  
                     print(f"Найдена пометка 'End' на странице {page_num + 1}. Завершение документа.")
-                    update_word_table(word_path, keywords, found_keywords, found_date)
+                    end_page = page_num + 1  # Конечная страница текущего документа
+                    update_word_table(word_path, keywords, found_keywords, found_date, start_page, end_page)
                     found_keywords = []
-                    found_date = None        
+                    found_date = None
+                    start_page = page_num + 2  # Начальная страница следующего документа       
 
         # Записываем информацию в файл Word после окончания обработки документа
-        
 
     return found_keywords, found_date
 
 
-def update_word_table(word_path, keywords, found_keywords, found_date):
+
+def update_word_table(word_path, keywords, found_keywords, found_date, start_page, end_page):
     doc = Document(word_path)
     table = doc.tables[0]
 
@@ -77,6 +80,10 @@ def update_word_table(word_path, keywords, found_keywords, found_date):
         if cell.text.strip() == "Наименование документа":
             column_index = cell._element.getparent().index(cell._element)
             break
+    for cell in table.rows[0].cells:
+        if cell.text.strip() == "Номера листов":
+            list_index = cell._element.getparent().index(cell._element)
+            break   
 
     # Добавляем новую строку в таблицу
     new_row_index = len(table.rows)
@@ -94,6 +101,7 @@ def update_word_table(word_path, keywords, found_keywords, found_date):
                 continue
             
             key_description = keywords.get(found_keyword)
+            
             if key_description is None:
                 print(f"Описание для ключа '{found_keyword}' не найдено.")
                 continue
@@ -107,7 +115,12 @@ def update_word_table(word_path, keywords, found_keywords, found_date):
     else:
         cell.text += ""  # Добавляем пустую строку, если ключевые слова не найдены
 
+    # Добавляем диапазон страниц в столбец "Номера листов"
+    pages_range = f"{start_page}-{end_page}"
+    table.cell(new_row_index, list_index).text = pages_range
+
     doc.save(word_path)
+
 
 
 def process_image(image_path, keywords, word_path):
@@ -159,6 +172,7 @@ def read_keys(keys_path):
     for row in table.rows[1:]:  # Пропускаем первую строку, так как это заголовок
         key = row.cells[0].text.strip()  # Берем текст из второй ячейки в строке (столбец "Значение ключа")
         key_description = row.cells[1].text.strip()  # Берем текст из третьей ячейки в строке (столбец "Описание ключа")
+        
         keys[key] = key_description
         print(f"Добавлен Ключ: '{key}' С описанием: '{key_description}'")
     return keys
